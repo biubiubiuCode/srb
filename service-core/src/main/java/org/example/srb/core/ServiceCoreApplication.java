a6bee37b890cc433cb31bc758c1f7aa477ab8ade
hex <1439055320@qq.com>
2024-04-01 19:53:31 +0800

    pom文件配置

---
+++ service-core/src/main/java/org/example/srb/core/ServiceCoreApplication.java
@@ -0,0 +1,22 @@
+package org.example.srb.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * ClassName: org.example.srb.core.ServiceCoreApplication
+ * Description:
+ * $
+ *
+ * @author wendao
+ * @version 1.0
+ * @Create 2024/04/01 19:40
+ **/
+@SpringBootApplication
+@ComponentScan("org.example.srb")
+public class ServiceCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+}
---
+++ service-core/src/main/java/org/example/srb/core/config/MyBatisPlusConfig.java
@@ -0,0 +1,26 @@
+package org.example.srb.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author wendao
+ * @since 2024-04-01
+ **/
+@Configuration
+@MapperScan("org.example.srb.core.mapper")
+@EnableTransactionManagement //引入事务的Bean
+public class MyBatisPlusConfig {
+    //分页拦截器管理
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页
+        return interceptor;
+    }
+}
